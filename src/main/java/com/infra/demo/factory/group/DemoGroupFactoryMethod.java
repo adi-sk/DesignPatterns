b4059f2c9b337bb1c8f04879b06b7ec3657eecf0
Aditya Kurhade <adikrhd@gmail.com>
2020-07-08 13:11:04 +0530

    factory is grouped under different class.

---
+++ src/main/java/com/infra/demo/factory/group/DemoGroupFactoryMethod.java
@@ -0,0 +1,27 @@
+package com.infra.demo.factory.group;
+
+//here we will have factory methods in the different class but at the same time access to the constructors
+public class DemoGroupFactoryMethod {
+    public static void main(String[] args) {
+        Point.PointFactory.newCartesianPoint(2,3);
+        Point.PointFactory.newPolarPoint(3,230);
+    }
+}
+
+class Point {
+    private double x, y;
+    private Point(double x, double y) { // made constructor private
+        this.x = x;
+        this.y = y;
+    }
+
+   public static class PointFactory{
+       public static Point newCartesianPoint(double x, double y) {
+           return new Point(x, y);
+       }
+
+       public static Point newPolarPoint(double rho, double theta){
+           return new Point(rho*Math.cos(theta),rho*Math.sin(theta));
+       }
+   }
+}
